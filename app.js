ed71feb6123f65ada0406716b4f6da9c9c967775
Riki Goto <rgot0005@student.monash.edu>
2025-08-03 18:06:44 +0800

    feat; impelmented the func of completed task

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-$(document).ready(function() {
-    $("#addTodo").click(function() {
-        const inputTodo = $("#todoInput").val();
-        
-        // 入力値が空でないことを確認
-        if (inputTodo.trim() !== "") {
-            $("#todolist").append("<li><input type='checkbox'> " + inputTodo + "</li>");
-            $("#todoInput").val(""); // Clear the input field after adding the todo
-        }
-    });
+$("#addTodo").click(function() {
+    const inputTodo = $("#todoInput").val();
+    $("#todolist").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    $("#todoInput").val(""); // Clear the input field after adding the todo
+});
+
+
+
+// チェックボックスの状態変更を監視
+$(document).on('change', 'input[type=checkbox]', function() {
+    if ($(this).is(':checked')) {
+        $(this).parent().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+    }
 });
